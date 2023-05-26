50b1720a62909ab82854d4b09a9a39343a1da5e8
Gonca Negis <gonca.negis@gmail.com>
2023-05-26 14:54:22 +0300

    added form, messages array rendering to chat page

--- pages/chat/[[...chatId]].js
+++ pages/chat/[[...chatId]].js
@@ -1,6 +1,50 @@
+import { ChatSidebar } from "components/ChatSidebar";
 import Head from "next/head";
+import { useState } from "react";
+import { streamReader } from "openai-edge-stream";
+import { v4 as uuid } from "uuid";
+import { Message } from "components/Message";
 
-export default function Home() {
+export default function ChatPage() {
+  const [incomingMessage, setIncomingMessage] = useState("");
+  const [messageText, setMessageText] = useState("");
+  const [newChatMessages, setNewChatMessages] = useState([]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setNewChatMessages((prev) => {
+      const newChatMessages = [
+        ...prev,
+        {
+          _id: uuid(),
+          role: "user",
+          content: messageText,
+        },
+      ];
+      return newChatMessages;
+    });
+
+    const response = await fetch(`api/chat/sendMessage`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        message: messageText,
+      }),
+    });
+
+    const data = response.body;
+    if (!data) return;
+
+    const reader = data.getReader();
+    await streamReader(reader, (message) => {
+      console.log("message: ", message);
+      setIncomingMessage((s) => `${s}${message.content}`);
+    });
+  };
+
   return (
     <>
       <Head>
@@ -8,10 +52,38 @@ export default function Home() {
       </Head>
 
       <div className="grid h-screen grid-cols-[260px_1fr]">
-        <aside>sidebar</aside>
+        <ChatSidebar />
         <div className="flex flex-col bg-gray-700">
-          <div className="flex-1">chat window</div>
-          <footer className="bg-gray-800 p-10">footer</footer>
+          <div className="flex-1 text-white">
+            {newChatMessages.map((message) => (
+              <Message
+                key={message._id}
+                role={message.role}
+                content={message.content}
+              />
+            ))}
+
+            {!!incomingMessage && (
+              <Message role="assistant" content={incomingMessage} />
+            )}
+          </div>
+          <footer className="bg-gray-800 p-10">
+            <form onSubmit={handleSubmit}>
+              <fieldset className="flex gap-2">
+                <textarea
+                  value={messageText}
+                  onChange={(e) => setMessageText(e.target.value)}
+                  placeholder="Send a message..."
+                  className="w-full resize-none rounded-md bg-gray-700 p-2 text-white focus:border-emerald-500 focus:bg-gray-600 focus:outline focus:outline-emerald-500"
+                  name=""
+                  id=""
+                ></textarea>
+                <button type="submit" className="btn">
+                  Send
+                </button>
+              </fieldset>
+            </form>
+          </footer>
         </div>
       </div>
     </>
