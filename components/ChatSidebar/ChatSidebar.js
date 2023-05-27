cfe9ff0b7509e18091b75aa08cdb86e49dde570f
Gonca Negis <gonca.negis@gmail.com>
2023-05-27 23:21:30 +0300

    added chatlist links, styling to sidebar

--- components/ChatSidebar/ChatSidebar.js
+++ components/ChatSidebar/ChatSidebar.js
@@ -1,23 +1,61 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMessage,
+  faPlus,
+  faRightFromBracket,
+} from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
-export const ChatSidebar = () => {
+export const ChatSidebar = ({ chatId }) => {
+  const [chatList, setChatList] = useState([]);
+
   useEffect(() => {
     const loadChatList = async () => {
-      const response = await fetch(`api/chat/getChatList`, {
+      const response = await fetch(`/api/chat/getChatList`, {
         method: "POST",
       });
 
       const json = await response.json();
       console.log("chat list: ", json);
+      setChatList(json?.chats || []);
     };
 
     loadChatList();
-  }, []);
+  }, [chatId]);
 
   return (
-    <aside className="bg-gray-900 text-white">
-      <Link href="/api/auth/logout">Logout</Link>
+    <aside className="bg-gray-900 text-white flex flex-col overflow-hidden">
+      <Link
+        href="/chat"
+        className="side-menu-item bg-emerald-500 hover:bg-emerald-600"
+      >
+        <FontAwesomeIcon icon={faPlus} />
+        <span>New Chat</span>
+      </Link>
+      <div className="flex-1 overflow-auto bg-gray-950">
+        {chatList.map((chat) => (
+          <Link
+            key={chat._id}
+            href={`/chat/${chat._id}`}
+            className={`side-menu-item ${
+              chatId === chat._id ? "bg-gray-700 hover:bg-gray-700" : ""
+            }`}
+          >
+            <FontAwesomeIcon icon={faMessage} />
+            <span
+              title={chat.title}
+              className="overflow-hidden text-ellipsis whitespace-nowrap"
+            >
+              {chat.title}
+            </span>
+          </Link>
+        ))}
+      </div>
+      <Link href="/api/auth/logout" className="side-menu-item">
+        <FontAwesomeIcon icon={faRightFromBracket} />
+        Logout
+      </Link>
     </aside>
   );
 };
