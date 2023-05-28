5b7105763e663aedc4dc16dd8de9b8ea9b39ce21
Gonca Negis <gonca.negis@gmail.com>
2023-05-28 22:49:40 +0300

    if endpoint gets chatid, adds message to chat array. if not, creates a new chat array.

--- pages/api/chat/sendMessage.js
+++ pages/api/chat/sendMessage.js
@@ -6,7 +6,9 @@ export const config = {
 
 export default async function handler(req) {
   try {
-    const { message } = await req.json();
+    const { chatId: chatIdFromParam, message } = await req.json();
+
+    let chatId = chatIdFromParam;
 
     const initialChatMessage = {
       role: "system",
@@ -14,23 +16,64 @@ export default async function handler(req) {
         "Your name is Chatty Pete. You're an incredibly intelligent and quick-thinking AI, that always replies with an enthusiastic and positive energy. You were created by Gonca Negis. Your response must be formatted as markdown.",
     };
 
-    const response = await fetch(
-      `${req.headers.get("origin")}/api/chat/createNewChat`,
-      {
-        method: "POST",
-        headers: {
-          "content-type": "application/json",
-          cookie: req.headers.get("cookie"),
-        },
-        body: JSON.stringify({
-          message,
-        }),
+    let newChatId;
+    let chatMessages = [];
+
+    if (chatId) {
+      // add message to chat
+      const response = await fetch(
+        `${req.headers.get("origin")}/api/chat/addMessageToChat`,
+        {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+            cookie: req.headers.get("cookie"),
+          },
+          body: JSON.stringify({
+            chatId,
+            role: "user",
+            content: message,
+          }),
+        }
+      );
+      const json = await response.json();
+      chatMessages = json.chat.messages || [];
+    } else {
+      const response = await fetch(
+        `${req.headers.get("origin")}/api/chat/createNewChat`,
+        {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+            cookie: req.headers.get("cookie"),
+          },
+          body: JSON.stringify({
+            message,
+          }),
+        }
+      );
+
+      const json = await response.json();
+
+      chatId = json._id;
+      newChatId = json._id;
+      chatMessages = json.messages || [];
+    }
+
+    const messagesToInclude = [];
+    chatMessages.reverse();
+    let usedTokens = 0;
+    for (let chatMessage of chatMessages) {
+      const messageTokens = chatMessage.content.length / 4;
+      usedTokens = usedTokens + messageTokens;
+      if (usedTokens <= 2000) {
+        messagesToInclude.push(chatMessage);
+      } else {
+        break;
       }
-    );
+    }
 
-    const json = await response.json();
-
-    const chatId = json._id;
+    messagesToInclude.reverse();
 
     const stream = await OpenAIEdgeStream(
       "https://api.openai.com/v1/chat/completions",
@@ -42,19 +85,15 @@ export default async function handler(req) {
         method: "POST",
         body: JSON.stringify({
           model: "gpt-3.5-turbo",
-          messages: [
-            initialChatMessage,
-            {
-              content: message,
-              role: "user",
-            },
-          ],
+          messages: [initialChatMessage, ...messagesToInclude],
           stream: true,
         }),
       },
       {
         onBeforeStream: ({ emit }) => {
-          emit(chatId, "newChatId");
+          if (newChatId) {
+            emit(chatId, "newChatId");
+          }
         },
         onAfterStream: async ({ emit, fullContent }) => {
           await fetch(
